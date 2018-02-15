2fb99792d810de18cbde729ae2d753213d4db77c
maria <maria.belyalova@b-monitor.ru>
2018-02-15 20:50:34 +0300

    init

---
+++ Lab2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using static ImageReadCS.Lab1;
+
+namespace ImageReadCS
+{
+    public static class Lab2
+    {
+        static GrayscaleFloatImage RgbToGray(ColorFloatImage image)
+        {
+            GrayscaleFloatImage gray = new GrayscaleFloatImage(image.Width, image.Height);
+
+            for (int y = 0; y < image.Height; y++)
+                for (int x = 0; x < image.Width; x++)
+                {
+                    gray[x, y] = (float)(image[x, y].r * 0.299 + image[x, y].g * 0.587 + image[x, y].b * 0.114);
+                }
+            return gray;
+        }
+
+        static float ColorPixelToGray(ColorFloatPixel p)
+        {
+            return (float)(p.r * 0.299 + p.g * 0.587 + p.b * 0.114);
+        }
+
+        public static double MSE(ColorFloatImage i1, ColorFloatImage i2)
+        {
+            if (i1.Height != i2.Height || i1.Width != i2.Width)
+                return 0;
+            
+            double mse = 0;
+
+            for (int y = 0; y < i1.Height; y++)
+                for (int x = 0; x < i1.Width; x++)
+                {
+                    mse += Math.Pow(ColorPixelToGray(i1[x, y]) - ColorPixelToGray(i2[x, y]), 2);
+                }
+
+            return mse / (i1.Width * i1.Height * 3);
+        }
+
+        public static double PSNR(ColorFloatImage i1, ColorFloatImage i2)
+        {
+            return 20 * Math.Log10(255.0 / Math.Sqrt(MSE(i1, i2)));
+        }
+
+        public static double SSIM(ColorFloatImage i1, ColorFloatImage i2)
+        {
+            double K1 = 0.01, K2 = 0.03;
+            int L = 255;
+
+            var C1 = Math.Pow(K1 * L, 2);
+            var C2 = Math.Pow(K2 * L, 2);
+            var mu1 = 0;//signal.fftconvolve(window, img1, mode = 'valid');
+            var mu2 = 0;//signal.fftconvolve(window, img2, mode = 'valid');
+            var mu1_sq = mu1 * mu1;
+            var mu2_sq = mu2 * mu2;
+            var mu1_mu2 = mu1 * mu2;
+            var sigma1_sq = 0;//signal.fftconvolve(window, img1 * img1, mode = 'valid') - mu1_sq;
+            var sigma2_sq = 0;//signal.fftconvolve(window, img2 * img2, mode = 'valid') - mu2_sq;
+            var sigma12 = 0;//signal.fftconvolve(window, img1 * img2, mode = 'valid') - mu1_mu2;
+
+            return ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                                (sigma1_sq + sigma2_sq + C2));
+            
+        }
+        public static double MSSIM(ColorFloatImage i1, ColorFloatImage i2)
+        {
+            return 0;
+        }
+
+        public static double Canny(ColorFloatImage i1, float sigma)
+        {
+            GrayscaleFloatImage gray = RgbToGray(i1);
+
+
+            return 0;
+
+        }
+        public static double Gabor(ColorFloatImage i1)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static ImageReadCS.Lab1;
+using static ImageReadCS.Lab2;
+
+namespace ImageReadCS
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			string path = Directory.GetCurrentDirectory();
+
+			if ( args.Length < 2 )
+			{
+				Console.WriteLine( "Empty arguments" );
+				return;
+			}
+
+			string inputFileName = String.Concat( new List<string>() { "", args[ 0 ], ".bmp" } );
+			string outputFileName = String.Empty;
+			List<string> outputName = new List<string>() { "", "", "", "", ".bmp" };
+
+			if ( !File.Exists( inputFileName ) )
+			{
+				Console.WriteLine( "File doesn't exist" );
+				return;
+			}
+
+			ColorFloatImage image = ImageIO.FileToColorFloatImage( inputFileName );
+
+			switch ( args[ 2 ] )
+			{
+				case "invert":
+					image = InvertImage( image );
+					outputName[ 1 ] = "invert";
+					break;
+
+				case "mirror":
+					outputName[ 1 ] = "mirror";
+					switch ( args[ 3 ] )
+					{
+						case "x":
+							outputName[ 2 ] = "_x";
+							image = MirrorX( image );
+							break;
+						case "y":
+							outputName[ 2 ] = "_y";
+							image = MirrorY( image );
+							break;
+					}
+					break;
+
+				case "rotate":
+					outputName[ 1 ] = "rotate";
+					string num = args[ 4 ].Trim( new char[] { '(', ')' } );
+					outputName[ 2 ] = $"_{num}";
+					int angle = 0;
+					int.TryParse( num, out angle );
+
+					switch ( args[ 3 ] )
+					{
+						case "cw":
+							outputName[ 3 ] = "_cw";
+							image = RotateCW( image, angle );
+							break;
+						case "ccw":
+							outputName[ 3 ] = "_ccw";
+							image = RotateCW( image, 360 - angle );
+							break;
+					}
+					break;
+
+				case "prewitt":
+					outputName[ 1 ] = "prewitt";
+					image = Prewitt( image, args[ 3 ] );
+					break;
+
+				case "sobel":
+					outputName[ 1 ] = "sobel";
+					image = Sobel( image, args[ 3 ] );
+					break;
+
+				case "roberts":
+					outputName[ 1 ] = "roberts";
+					string rob = args[ 3 ].Trim( new char[] { '(', ')' } );
+					outputName[ 2 ] = $"_{rob}";
+					int rob_param = 1;
+					int.TryParse( rob, out rob_param );
+					image = Roberts( image, rob_param );
+					break;
+
+				case "median":
+					outputName[ 1 ] = "median";
+					string med = args[ 3 ].Trim( new char[] { '(', ')' } );
+					outputName[ 2 ] = $"_{med}";
+					int rad = 1;
+					int.TryParse( med, out rad );
+					image = Median( image, rad );
+					break;
+
+				case "gauss":
+					outputName[ 1 ] = "gauss";
+					string g = args[ 3 ].Trim( new char[] { '(', ')' } );
+					outputName[ 2 ] = $"_{g}";
+					float sigma = 1;
+					float.TryParse( g, out sigma );
+					image = Gauss( image, sigma );
+					break;
+			}
+
+			outputFileName = String.Concat( outputName );
+			ImageIO.ImageToFile( image, outputFileName );
+		}
+	}
+}
