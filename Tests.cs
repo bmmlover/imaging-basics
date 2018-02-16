196724bdd9ce85a998d714e4b6118e0565215f9d
maria <maria.belyalova@b-monitor.ru>
2018-02-16 14:33:50 +0300

    new test

--- Tests.cs
+++ Tests.cs
@@ -11,7 +11,63 @@ namespace ImageReadCS
 	[TestFixture]
 	class Tests
 	{
+		public void SetDir()
+		{
+			var dir = Path.GetDirectoryName( typeof( Program ).Assembly.Location );
+			Environment.CurrentDirectory = dir;
+		}
+
+		public ColorFloatImage ReadImage()
+		{
+			string inputFileName = "input//mandarin.bmp";
+			if ( !File.Exists( inputFileName ) )
+			{
+				Console.WriteLine( "File doesn't exist" );
+				return null;
+			}
+			return ImageIO.FileToColorFloatImage( inputFileName );
+		}
+
+		public void SaveImage( ColorFloatImage image, string filename )
+		{
+			List<string> outputFileName = new List<string>() { "mandarin_", "", ".bmp" };
+			outputFileName[ 1 ] = filename;
+			ImageIO.ImageToFile( image, String.Concat( outputFileName ) );
+		}
+
 		[Test]
+		public void TestRotation()
+		{
+			SetDir();
+
+			List<int> angles = new List<int>() { 0, 90, 180, 270, 45, 135, 225, 315, 360, 405, -45 };
+
+			foreach (var angle in angles )
+			{
+				ColorFloatImage image = ReadImage();
+				image = RotateCW( image, angle );
+				SaveImage( image, angle.ToString() );
+			}
+		}
+
+		[Test]
+		public void Test()
+		{
+			var dir = Path.GetDirectoryName( typeof( Program ).Assembly.Location );
+			Environment.CurrentDirectory = dir;
+
+			string[] fileEntries = Directory.GetFiles( Directory.GetCurrentDirectory() );
+			string inputFileName = "input//mandarin.bmp";
+			List<string> outputFileName = new List<string>() { "mandarin_", "", ".bmp" };
+			List<int> angles = new List<int>() { 0, 90, 180, 270, 45, 135, 225, 315, 360, 405, -45 };
+
+			if ( !File.Exists( inputFileName ) )
+			{
+				Console.WriteLine( "File doesn't exist" );
+				return;
+			}
+
+					[Test]
 		public void TestRotation()
 		{
 			var dir = Path.GetDirectoryName( typeof( Program ).Assembly.Location );
@@ -28,13 +84,10 @@ namespace ImageReadCS
 				return;
 			}
 
-			foreach (var angle in angles )
-			{
-				ColorFloatImage image = ImageIO.FileToColorFloatImage( inputFileName );
-				image = RotateCW( image, angle );
-				outputFileName[ 1 ] = angle.ToString();
-				ImageIO.ImageToFile( image, String.Concat( outputFileName ) );
-			}
+			ColorFloatImage image = ImageIO.FileToColorFloatImage( inputFileName );
+			image = RotateCW( image, angle );
+			outputFileName[ 1 ] = angle.ToString();
+			ImageIO.ImageToFile( image, String.Concat( outputFileName ) );
 		}
 	}
 }
